3c93450baac9571a0905e5bd19988797b8cdb5d6
uchrissd <uchrissd@protonmail.com>
2019-10-17 10:05:01 -0700

    made some improvements to the HTML document to make it easier to target the HTML elements, updated the js with a new function to store values

--- scripts/script.js
+++ scripts/script.js
@@ -1,47 +1,16 @@
 //This is the main JavaScript file for the work day planner
 
-//Function generate table rows
+//Event listener for the save button
 
-function makeWorkPlanner(time) {
-  var table = $("<table>");
-  var tableBody = $("<tbody>");
-  var tableRow = $("<tr>");
-  var timeColumn = $("<td>" + time + "</td>");
-  var inputColumn = $("<td>");
-  var saveColumn = $("<td>");
-  var saveButton = $("<button>" + "Save" + "</button>");
-  var userInput = $("<textarea>");
+$("button:submit").bind("click", function() {
+  //This variable stores the user input
+  var userInput = $(this)
+    .closest("tr")
+    .find("input:text")
+    .val();
+  //This variable stores the corresponding hour of time
+  var time = $(this).data("hour");
 
-  var tableContainer = $(".container");
-  tableContainer.append(table);
-  table.append(tableBody);
-  tableBody.append(tableRow);
-
-  saveColumn.append(saveButton);
-  inputColumn.append(userInput);
-
-  tableRow.append(timeColumn);
-  tableRow.append(inputColumn);
-  tableRow.append(saveColumn);
-
-  console.log(table);
-  console.log(tableBody);
-  console.log(tableRow);
-  console.log(timeColumn);
-  console.log(inputColumn);
-  console.log(saveColumn);
-}
-
-makeWorkPlanner("9.am.");
-makeWorkPlanner("10a.m.");
-makeWorkPlanner("11a.m.");
-makeWorkPlanner("12p.m.");
-makeWorkPlanner("13p.m.");
-makeWorkPlanner("14p.m.");
-makeWorkPlanner("15p.m.");
-makeWorkPlanner("16p.m.");
-makeWorkPlanner("17p.m.");
-makeWorkPlanner("18p.m.");
-makeWorkPlanner("19p.m.");
-makeWorkPlanner("20p.m.");
-makeWorkPlanner("21p.m.");
+  console.log(userInput);
+  console.log(time);
+});
