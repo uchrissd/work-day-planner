6592962059cb30b1cc3fb70fc74eceaef5faa349
uchrissd <uchrissd@protonmail.com>
2019-10-18 12:55:08 -0700

    updated the index file with the moment js time, added the moment js file to the project foler, updated the js script with additional local storage logic

--- scripts/script.js
+++ scripts/script.js
@@ -1,16 +1,46 @@
 //This is the main JavaScript file for the work day planner
 
-//Event listener for the save button
+//This function takes in and displays the local date and time in real-time using moment.js
+function realTime() {
+  var dateTime = moment().format("LLLL");
+  console.log(dateTime);
+  $("#currentDay").append(dateTime);
+}
+realTime();
+console.log(realTime);
 
-$("button:submit").bind("click", function() {
+//This function acts as an event lister. When any save button is clicked, the user input and the corresponding hour is saved to local storage
+
+$("button:submit").on("click", function() {
+  var storageVariable = localStorage.getItem("userArray");
+  storageVariable = JSON.parse(storageVariable);
+  console.log(storageVariable);
+  if (!storageVariable) {
+    storageVariable = [];
+  }
+
   //This variable stores the user input
+
   var userInput = $(this)
     .closest("tr")
     .find("input:text")
     .val();
-  //This variable stores the corresponding hour of time
-  var time = $(this).data("hour");
+  //This variable stores the hour of time
+  var hour = $(this).data("hour");
+
+  var userObject = { text: userInput, time: hour };
+
+  storageVariable.push(userObject);
+
+  localStorage.setItem("userArray", JSON.stringify(storageVariable));
 
   console.log(userInput);
-  console.log(time);
+  console.log(hour);
+  console.log(userObject);
 });
+
+//To-do list
+//Save user input to local storage
+//Access user input from local storage
+//Clear text iput area after user presses save button
+//
